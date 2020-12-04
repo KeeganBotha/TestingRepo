63b8433c29d7e29d5e1ad3678ed09014e80a2d41
Keegan Botha <keegan@ix.co.za>
2020-12-04 08:07:10 +0200

    switch statements

--- Grade Book Project/Grade Book Project/Book.cs
+++ Grade Book Project/Grade Book Project/Book.cs
@@ -20,16 +20,29 @@ namespace GradeBook
 			}
 			else 
 			{
-				Console.WriteLine("Invalid value");
+				throw new ArgumentException($"Invalid {nameof(grade)}");
 			}
 		}
 		public void AddLetterGrade(char letter)
 		{
-			if (letter == 'A')
+			switch(letter) 
 			{
-				AddGrade(90);			
+				case 'A':
+					AddGrade(90);
+					break;
+				case 'B':
+					AddGrade(80);
+					break;
+				case 'C':
+					AddGrade(70);
+					break;
+				case 'D':
+					AddGrade(60);
+					break;
+				default:
+					AddGrade(0);
+					break;
 			}
-
 		}
 		public Statistics ComputeStatistics() 
 		{
@@ -51,6 +64,22 @@ namespace GradeBook
 			} 
 
 			result.Average /= grades.Count;
+
+			switch (result.Average) 
+			{
+				case var d when d >= 90.00:
+					result.Letter = 'A';
+					break;
+				case var d when d >= 80.00:
+					result.Letter = 'B';
+					break;
+				case var d when d >= 70.00:
+					result.Letter = 'C';
+					break;
+				default:
+					result.Letter = 'F';
+					break;
+			}
 			return result;
 		}
 
--- Grade Book Project/Grade Book Project/Program.cs
+++ Grade Book Project/Grade Book Project/Program.cs
@@ -9,15 +9,32 @@ namespace GradeBook
 		static void Main(string[] args)
 		{
 			var book = new Book("Book One");
-			book.AddGrade(79.1);
-			book.AddGrade(89.1);
-			book.AddGrade(99.1);
+
+			do
+			{
+				var input = Console.ReadLine();
+				if (input == "q" || input == "Q")
+				{
+					break;
+				}
+				try
+				{
+					var grade = double.Parse(input);
+					book.AddGrade(grade);
+				}
+				catch(Exception err)
+				{
+					Console.WriteLine(err.Message);
+				}
+				
+			} while(1 < 100);
 
 			var stats = book.ComputeStatistics();
 
 			Console.WriteLine($"The lowest grade is {stats.Low}");
 			Console.WriteLine($"The highest grade is {stats.High}");
 			Console.WriteLine($"The average grade is {stats.Average:N1}");
+			Console.WriteLine($"The letter grade is {stats.Letter}");
 		}
 	}
 }
